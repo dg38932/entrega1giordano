c5b5d8e3034ee4dba110e8ece34cda6ddb0a5b7f
dg38932 <dg38932@hotmail.com>
2022-06-29 20:39:06 -0300

    commit #5

--- AppCoder/admin.py
+++ AppCoder/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
+
+admin.site.register(Empleados)
+admin.site.register(Clientes)
+admin.site.register(Productos)
 
 # Register your models here.
--- AppCoder/urls.py
+++ AppCoder/urls.py
@@ -3,7 +3,7 @@ from AppCoder import views
 
 urlpatterns = [
 
-    path('inicio',views.inicio, name="Inicio"),
+    path('',views.inicio, name="Inicio"),
     path('empleados', views.empleados, name="Empleados"),
     path('clientes', views.clientes, name="Clientes"),
     path('productos', views.productos, name="Productos"),
